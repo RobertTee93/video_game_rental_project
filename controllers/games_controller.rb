d930ad004be2216af5c42196bd50a188fef3898b
Robert Templeton <roberttee1993@googlemail.com>
2019-02-01 17:44:16 +0000

    added delete game functionality to app

--- controllers/games_controller.rb
+++ controllers/games_controller.rb
@@ -6,3 +6,9 @@ get "/games" do
   @games = Game.all
   erb( :"games/index" )
 end
+
+post "/games/:id/delete" do
+  @game = Game.find_by_id(params["id"])
+  @game.delete()
+  erb( :"games/delete" )
+end
