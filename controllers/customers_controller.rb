d91785fb9753c64ee9285549c79e7f9f4b3fbd3a
Robert Templeton <roberttee1993@googlemail.com>
2019-02-01 15:35:14 +0000

    added functionality to add customers from app

--- controllers/customers_controller.rb
+++ controllers/customers_controller.rb
@@ -6,3 +6,13 @@ get "/customers" do
   @customers = Customer.all()
   erb( :"customers/index" )
 end
+
+get "/customers/new" do
+  erb( :"customers/new")
+end
+
+post "/customers/" do
+  customer = Customer.new(params)
+  customer.save()
+  redirect( "/customers" )
+end
