088f655e336b58783626ebeede385f84c6e00bf1
Robert Templeton <roberttee1993@googlemail.com>
2019-02-01 15:03:23 +0000

    Added index home page and customer page

---
+++ app.rb
@@ -0,0 +1,10 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative("controllers/customers_controller")
+require_relative("controllers/games_controller")
+require_relative("controllers/rentals_controller")
+
+
+get "/" do
+  erb( :index )
+end
---
+++ controllers/customers_controller.rb
@@ -0,0 +1,8 @@
+require_relative("../models/Customer")
+
+also_reload("./models/*")
+
+get "/customers" do
+  @customers = Customer.all
+  erb( :"customers/index" )
+end
