73cd415721d7332c5bfad007c9d7f64102a1c9df
Robert Templeton <roberttee1993@googlemail.com>
2019-02-05 14:47:32 +0000

    added more games to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,11 +44,59 @@ game5 = Game.new({
   })
 
 game6 = Game.new({
-  "name" => "Skyrim",
+  "name" => "The Ledgend of Zelda: Breath of the Wild",
+  "genre" => "Adventure",
+  "platform" => "Nintendo Switch",
+  "release_date" => "2017-03-03",
+  "box_art" => "https://images-na.ssl-images-amazon.com/images/I/81VNAXO6PmL._SY445_.jpg"
+  })
+
+game7 = Game.new({
+  "name" => "Resident Evil 2",
+  "genre" => "Survival/Horror",
+  "platform" => "PS4",
+  "release_date" => "2019-01-25",
+  "box_art" => "https://images-na.ssl-images-amazon.com/images/I/915dUNOufyL._AC_SX430_.jpg"
+  })
+
+game8 = Game.new({
+  "name" => "Kingdom Hearts 3",
   "genre" => "RPG",
   "platform" => "Xbox One",
-  "release_date" => "2011-11-11",
-  "box_art" => "https://i5.walmartimages.com/asr/62bb88c6-6ea8-41a9-aad5-83329b48536f_1.d244b214da6e95a60ab40f833e27d43f.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF"
+  "release_date" => "2019-01-25",
+  "box_art" => "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6255/6255212_sd.jpg;maxHeight=640;maxWidth=550"
+  })
+
+game9 = Game.new({
+  "name" => "Minecraft",
+  "genre" => "Sandbox",
+  "platform" => "Xbox One",
+  "release_date" => "2009-05-17",
+  "box_art" => "https://www.virginmegastore.ae/medias/sys_master/root/h81/hc4/9109707259934/Minecraft-Xbox-One-Edition-401404-Detail.jpg"
+  })
+
+game10 = Game.new({
+  "name" => "Fifa 19",
+  "genre" => "Sport",
+  "platform" => "PS4",
+  "release_date" => "2018-09-28",
+  "box_art" => "https://icdn3.digitaltrends.com/image/fifa-19-game-art-ps4-720x720.jpg"
+  })
+
+game11 = Game.new({
+  "name" => "Crash Bandicoot N. Sane Trilogy",
+  "genre" => "Platformer",
+  "platform" => "PS4",
+  "release_date" => "2017-06-30",
+  "box_art" => "https://images-na.ssl-images-amazon.com/images/I/91iP4NOH5yL._SX342_.jpg"
+  })
+
+game12 = Game.new({
+  "name" => "Spyro Reignited Trilogy",
+  "genre" => "Platformer",
+  "platform" => "Xbox One",
+  "release_date" => "2018-11-13",
+  "box_art" => "https://images-na.ssl-images-amazon.com/images/I/81jZEz72nPL._SY445_.jpg"
   })
 
 game1.save
@@ -57,6 +105,12 @@ game3.save
 game4.save
 game5.save
 game6.save
+game7.save
+game8.save
+game9.save
+game10.save
+game11.save
+game12.save
 
 customer1 = Customer.new({
   "first_name" => "Thomas",
